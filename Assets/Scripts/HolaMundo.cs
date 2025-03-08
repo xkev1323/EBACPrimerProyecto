1f12f3e142fed0354f3483a7b8ad24763024f24f
Kevin Hernandez <kevin.alfonso.hernandez.amaya@gmail.com>
2025-03-08 13:04:48 -0800

    Primer Script
    Se incorporo el primer Script llamado Hola Mundo.
    Se uso el codigo Debug.Log, Debug.LogError, Debug.Warning para mostrar un mensaje a la consola.

---
+++ Assets/Scripts/HolaMundo.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HolaMundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hola Mundo");
+        Debug.LogWarning("Hola Warning");
+        Debug.LogError("Hola Error");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
