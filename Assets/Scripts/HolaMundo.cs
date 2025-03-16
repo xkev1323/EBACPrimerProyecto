764646e447c8a87f446082cc66a578f83a069de6
Kevin Hernandez <kevin.alfonso.hernandez.amaya@gmail.com>
2025-03-15 19:05:46 -0700

    Practica-Modulo-5
    Se incorpora el script HolaMundo.cs con las funciones del game loop de Unity que son:
    - Awake
    - Start
    - Update
    - FixedUpdate
    - LateUpdate
    
    Con las funciones LogError, LogWarning y Log de la clase debug para mostrar distintos tipos de Logs en la consola de Unity.

--- Assets/Scripts/HolaMundo.cs
+++ Assets/Scripts/HolaMundo.cs
@@ -2,17 +2,34 @@ using UnityEngine;
 
 public class HolaMundo : MonoBehaviour
 {
+    private void Awake()
+    {
+        Debug.Log("Estoy en Awake");
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         Debug.Log("Hola Mundo");
         Debug.LogWarning("Hola Warning");
         Debug.LogError("Hola Error");
+
+        Debug.Log("Estoy en Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.LogWarning("Estoy en Update");
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.LogWarning("Estoy en Fixed Update");
+    }
+
+    private void LateUpdate()
+    {
+        Debug.LogError("Estoy en Late Update");
     }
 }
