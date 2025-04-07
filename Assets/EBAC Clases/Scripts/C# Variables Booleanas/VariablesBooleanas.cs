24c07c3766fbade66f460221e844da064eed7b6d
Kevin Hernandez <kevin.alfonso.hernandez.amaya@gmail.com>
2025-04-06 21:07:47 -0700

    EBAC Clase C# Variables Booleanas
    Se modifico el Script de Booleanas jugando con las condicionales.

--- Assets/EBAC Clases/Scripts/C# Variables Booleanas/VariablesBooleanas.cs
+++ Assets/EBAC Clases/Scripts/C# Variables Booleanas/VariablesBooleanas.cs
@@ -2,15 +2,38 @@ using UnityEngine;
 
 public class VariablesBooleanas : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public bool variable1;
+    public bool variable2;
+    public bool variable3;
+    public int valor1 = 5;
 
-    // Update is called once per frame
-    void Update()
+    // A B C    OR  AND ((A || B) && C)
+    // 0 0 0    0   0       0
+    // 0 0 1    0   0       0
+    // 0 1 0    1   0       0
+    // 0 1 1    1   0       1
+    // 1 0 0    0   0       0
+    // 1 0 1    0   0       1
+    // 1 1 0    1   0       0
+    // 1 1 1    1   1       1
+
+    private void Start()
     {
-        
+        if ((variable1 || variable2) && variable3)
+        {
+            Debug.Log("La variable1 o la variable2 son verdaderas y la variable3 es verdadera");
+        }
+        else if (variable1 && !variable2)
+        {
+            Debug.Log("La variable1 es verdadera y la variable2 es falsa");
+        }
+        else if (variable1 && valor1 > 0)
+        {
+            Debug.Log("La variable1 es verdadera y el valor1 es mayor que 0");
+        }
+        else
+        {
+            Debug.Log("Ninguna de las condiciones anteriores se cumplió");
+        }
     }
 }
